906cfdbb2370699ec76ea72835df54f3a1a9f8a2
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-11-18 22:18:22 +0000

    sfsfdsfdsfldsfjdsf

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var favicon = require('serve-favicon');
 
 var app = express();
 
+app.use(favicon(__dirname + '/static/favicon.ico'));
 app.use('/static', express.static('static'));
 app.use('/images', express.static('images'));
+app.use('/favicon', express.static('images'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
