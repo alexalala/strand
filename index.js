7d7166b672dd073ab2d852e4b468eba7b75834cc
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-11-18 23:59:56 +0000

    ssdfdsjfkjfdjlfk

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var favicon = require('serve-favicon');
 
 var app = express();
-
-app.use(favicon(__dirname + '/static/favicon.ico'));
 app.use('/static', express.static('static'));
 app.use('/images', express.static('images'));
 app.use('/favicon', express.static('images'));
@@ -14,14 +11,6 @@ app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
   res.render('home');
-});
-
-app.get('/about', function (req, res) {
-    res.render('about');
-});
-
-app.get('/contact', function (req, res) {
-    res.render('contact');
 });
 
 app.listen(3000, function () {
--- static/script.js
+++ static/script.js
@@ -1,8 +1,1 @@
 var s = skrollr.init();
-$(window).on('scroll touchmove', function () {
-        if($(document).scrollTop() > 300) {
-            $('body').addClass('scrolled');
-        } else {
-            $('body').removeClass('scrolled');
-        }
-});
